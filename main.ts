253de6ebe40d822a771e7e9eb69c7a65ae18b13d
leyvapaco <82705842+leyvapaco@users.noreply.github.com>
2022-05-11 19:03:44 +0200

    Cambios tras la primera sesión de la feria. Quito turbo para que el número de turbos restantes no confunda al conductor, que deja de ver el dorsal de su coche. Apago leds de colores cuando paro. Amplio tiempo marcha atrás. Aumento velocidad.

--- main.ts
+++ main.ts
@@ -1,14 +1,17 @@
 function arranca () {
-    velocidad = 35
+    velocidad = 80
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    luces.showColor(neopixel.colors(NeoPixelColors.Red))
     music.playMelody("A B A G A B A G ", 444)
+    personalizar()
     arrancado = true
 }
 function para () {
     maqueen.motorStop(maqueen.Motors.All)
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    luces.showColor(neopixel.colors(NeoPixelColors.Black))
     arrancado = false
 }
 radio.onReceivedString(function (receivedString) {
@@ -26,7 +29,7 @@ radio.onReceivedString(function (receivedString) {
         } else if (receivedString == "para") {
             para()
         } else if (receivedString == "turbo") {
-            usaTurbo()
+        	
         }
     } else if (receivedString == "arranca") {
         if (!(penalizado)) {
@@ -34,11 +37,9 @@ radio.onReceivedString(function (receivedString) {
         }
     }
     compruebaBorde()
-    basic.pause(200)
+    basic.pause(100)
 })
 function personalizar () {
-    luces = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
-    luces.showColor(neopixel.colors(NeoPixelColors.Purple))
     dorsal = 1
     basic.showNumber(dorsal)
     radio.sendNumber(dorsal)
@@ -49,44 +50,47 @@ function compruebaBorde () {
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 || maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
         para()
         penalizado = true
-        for (let index = 0; index < 8; index++) {
-            luces.showColor(neopixel.colors(NeoPixelColors.Red))
+        for (let index = 0; index < 6; index++) {
+            basic.clearScreen()
             music.setVolume(127)
-            music.playTone(330, music.beat(BeatFraction.Whole))
-            basic.pause(500)
-            luces.showColor(neopixel.colors(NeoPixelColors.Yellow))
-            basic.pause(500)
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+            music.playTone(392, music.beat(BeatFraction.Whole))
+            basic.pause(150)
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+            basic.showNumber(dorsal)
+            basic.pause(150)
         }
         penalizado = false
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 25)
-        basic.pause(1000)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 25)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 25)
-        basic.pause(1000)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, velocidad)
+        basic.pause(2000)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 30)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
+        basic.pause(1200)
         para()
     }
 }
 function usaTurbo () {
     if (turbos > 0) {
-        radio.sendNumber(turbos - 1)
-        basic.pause(100)
         music.playTone(523, music.beat(BeatFraction.Double))
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad * 3)
-        basic.pause(3000)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad * 4)
+        basic.pause(2000)
         music.playTone(175, music.beat(BeatFraction.Double))
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
         turbos += -1
     }
 }
 let dorsal = 0
-let luces: neopixel.Strip = null
 let penalizado = false
 let arrancado = false
 let velocidad = 0
 let turbos = 0
-radio.setGroup(80)
-music.stopAllSounds()
+let luces: neopixel.Strip = null
 basic.clearScreen()
+music.stopAllSounds()
+radio.setGroup(80)
+luces = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 para()
 personalizar()
-turbos = 6
+turbos = 10
